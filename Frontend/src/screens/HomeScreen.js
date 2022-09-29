19b988d4e81561a6aff64472fc3bd1a74a235182
ZaidUllakhan <80337763+ZaidUllakhan@users.noreply.github.com>
2022-09-29 22:00:45 +0530

    Add files via upload

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,5 +1,27 @@
+
+import data from "../data"
 const HomeScreen = {
     render: () => {
+        const { products} = data;
+        return `
+        <ul class="products">
+            ${products.map( product => `
+            <li>
+            <div class="product">
+            <a href="/#/product/${product.id}">
+                            <img src="${product.image_src}"/>
+                            <div class="product-brand">
+                                ${product.name}
+                            </div>
+                            <div class="product-price">
+                                $59
+                            </div>
+
+                        </div>
+            </li>
+            `).join('\n')}
+            `
         
     }
 }
+export default HomeScreen;
